099a4bdd90893b215f643e3dedaba333bcdc3770
mikeylevar13-ai <mikeylevar13@gmail.com>
2026-01-06 09:25:03 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "tapdodge-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
